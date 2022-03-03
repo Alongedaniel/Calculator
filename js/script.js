3e95898fe3c40e71f6fb0ea36da556b5442099d3
Alongedaniel <alongedaniel41@gmail.com>
2022-03-03 13:25:12 +0100

    Corrected and added new functions to the calculator with javascript

--- js/script.js
+++ js/script.js
@@ -1,22 +1,65 @@
-let outputScreen = document.querySelector(".calculator-input");
+// let outputScreen = document.querySelector(".calculator-input");
 // const numberKeys = document.querySelectorAll(".numbers")
+// const clearButton = document.getElementById("clear")
 
-function display(num) {
-  outputScreen.value += num;
-}
+// numberKeys.forEach(element =>{
+//   element.addEventListener("click", () => {
+//     outputScreen.value += element.innerHTML
+//   })
+// })
 
-function calculate() {
+// function calculate() {
+//   try {
+//     outputScreen.value = eval(outputScreen.value);
+//   } catch (err) {
+//     alert("Invalid");
+//   }
+// }
+
+// function clearInput() {
+//   console.log("Being called")
+//   outputScreen.value = "";
+// }
+
+// function del() {
+//   outputScreen.value = outputScreen.value.slice(0, -1);
+// }
+let outputScreen = document.querySelector(".calculator-input");
+const numberKeys = document.querySelectorAll(".numbers");
+const clearButton = document.getElementById("clear");
+const symbols = document.querySelectorAll(".symbols");
+const equalButton = document.querySelector(".equal");
+const bracket = document.querySelectorAll(".bracket");
+const deleteButton = document.getElementById("delete");
+
+numberKeys.forEach((element) => {
+  element.addEventListener("click", () => {
+    outputScreen.value += element.innerHTML;
+  });
+});
+
+clearButton.addEventListener("click", () => {
+  outputScreen.value = "";
+});
+
+symbols.forEach((symbol) => {
+  symbol.addEventListener("click", () => {
+    outputScreen.value += symbol.innerHTML;
+  });
+});
+equalButton.addEventListener("click", () => {
   try {
     outputScreen.value = eval(outputScreen.value);
   } catch (err) {
     alert("Invalid");
   }
-}
+});
+bracket.forEach((bracketButton) => {
+  bracketButton.addEventListener("click", () => {
+    outputScreen.value += bracketButton.innerHTML;
+  });
+});
 
-function clear() {
-  outputScreen.value = "";
-}
-
-function del() {
+deleteButton.addEventListener("click", () => {
   outputScreen.value = outputScreen.value.slice(0, -1);
-}
+});
